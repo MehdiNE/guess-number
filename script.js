7f3093eb1e0b0144627262fffab11aa97a7e7286
MehdiNE <mahdimahdi9731@gmail.com>
2021-02-05 18:19:32 +0330

    increase secretNumber

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
 console.log('welcome to the application');
-let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 50) + 1;
 let score = 20;
 let highscore = 0;
 
@@ -60,7 +60,7 @@ document.querySelector('.check').addEventListener('click', function () {
 
 document.querySelector('.again').addEventListener('click', function () {
   score = 20;
-  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  secretNumber = Math.trunc(Math.random() * 50) + 1;
 
   document.querySelector('.message').textContent = 'start guessing...';
   document.querySelector('.score').textContent = score;
